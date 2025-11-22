be8d6e9a2ec35858b713e4fb672078fc06250b0f
eduardoymd <eduardoeikiyamada@gmail.com>
2025-11-22 03:57:06 -0300

    fix

--- app/core/task_manager.py
+++ app/core/task_manager.py
@@ -5,8 +5,4 @@ class TaskManager:
         self.ai = GroqService(api_key="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")
 
     def organize(self, tasks, stress_score: int):
-        """
-        Envia a lista de tarefas para o serviço de IA
-        e retorna o JSON já organizado.
-        """
         return self.ai.organize_with_ai(tasks, stress_score)
